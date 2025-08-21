PDeXchange/pac

Automate event capping

cmd/event-notifier/main.go
@@ -9,6 +9,7 @@ import (
 	"github.com/PDeXchange/pac/internal/pkg/notifier/client/mail"
 	"github.com/PDeXchange/pac/internal/pkg/pac-go-server/db/mongodb"
 	log "github.com/PDeXchange/pac/internal/pkg/pac-go-server/logger"
+	"github.com/PDeXchange/pac/internal/pkg/pac-go-server/services"
 )
 
 var (
@@ -17,6 +18,8 @@ var (
 	serviceAccount         = os.Getenv("KEYCLOAK_SERVICE_ACCOUNT")
 	serviceAccountPassword = os.Getenv("KEYCLOAK_SERVICE_ACCOUNT_PASSWORD")
 )
+
+const cappedEvents = 30000
 
 func validateEnvVars() error {
 	globalVars := map[string]string{
@@ -49,6 +52,10 @@ func main() {
 		}
 	}
 	defer disconnect()
+	services.SetDB(db)
+	if err := services.SetEventCapping(cappedEvents); err != nil {
+		l.Fatal("Capping event notifier failed", zap.Error(err))
+	}
 	mailClient := mail.New()
 	notifier(db, mailClient)
 }

IBM/power-access-cloud

api/cmd/event-notifier/main.go
@@ -9,6 +9,7 @@ import (
 	"github.com/PDeXchange/pac/internal/pkg/notifier/client/mail"
 	"github.com/PDeXchange/pac/internal/pkg/pac-go-server/db/mongodb"
 	log "github.com/PDeXchange/pac/internal/pkg/pac-go-server/logger"
+	"github.com/PDeXchange/pac/internal/pkg/pac-go-server/services"
 )
 
 var (
@@ -17,6 +18,8 @@ var (
 	serviceAccount         = os.Getenv("KEYCLOAK_SERVICE_ACCOUNT")
 	serviceAccountPassword = os.Getenv("KEYCLOAK_SERVICE_ACCOUNT_PASSWORD")
 )
+
+const cappedEvents = 30000
 
 func validateEnvVars() error {
 	globalVars := map[string]string{
@@ -49,6 +52,10 @@ func main() {
 		}
 	}
 	defer disconnect()
+	services.SetDB(db)
+	if err := db.SetEventCapping(cappedEvents); err != nil {
+		l.Fatal("Capping event notifier failed", zap.Error(err))
+	}
 	mailClient := mail.New()
 	notifier(db, mailClient)
 }