d1880e97ef945e6cc06d9244d1746b447fee3849
Vikas <vikas.satyanarayana.bolla@ibm.com>
2026-03-17 15:44:13 +0530

    Add systemType information in catalog page
    Signed-off-by: Vikas <vikas.satyanarayana.bolla@ibm.com>

--- web/src/components/Catalogs.jsx
+++ web/src/components/Catalogs.jsx
@@ -144,9 +144,10 @@ const Catalogs = () => {
 </Tooltip>}
         <img src={row.image_thumbnail_reference} width="15%" height="auto" alt="centos" /><br/>
       <strong><em>{row.name}</em></strong><br/><br/>
-      
+        
 Cores: {row.capacity.cpu}<br/>
-Memory: {row.capacity.memory} GB<br/><br/>
+Memory: {row.capacity.memory} GB<br/>
+SystemType: {row.vm.system_type}<br/><br/>
 {/* {row.description}
 <br /><br /> */}
 <span style={{float:"right"}} >
